67af00b84bad907c86c538930cc9a7a1925a4a42
nekacurry <anneka498@gmail.com>
2020-11-16 18:52:51 -0600

    Create sprite.js

---
+++ resources/sprite.js
@@ -0,0 +1,30 @@
+// ===========================
+// SPRITE CLASS
+// ===========================
+
+class Sprite {
+  constructor(x = 0, y = 0, width = 10, height = 10, color='red') {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.color = color
+  }
+
+  moveTo(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  moveBy(dx, dy) {
+    this.x += dx
+    this.y += dy
+  }
+
+  render(ctx) {
+    ctx.beginPath()
+    ctx.rect(this.x, this.y, this.width, this.height)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
